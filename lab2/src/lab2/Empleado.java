15a1b41cb8a1c30e1efa191fe1c4300beaf50313
Andrea Martinez <andreamartinezisj28@gmail.com>
2025-10-24 09:13:44 -0600

    Clase de Empleado

--- lab2/src/lab2/Empleado.java
+++ lab2/src/lab2/Empleado.java
@@ -4,10 +4,44 @@
  */
 package lab2;
 
+import java.util.Calendar;
+
 /**
  *
  * @author CarlosXl
  */
 public class Empleado {
-    
+    protected int codigo;
+    String nombre;
+    Calendar fechacontra;
+    double salariobase;
+    double horastrabajadas;
+
+    public Empleado(int codigo, String nombre, double salariobase) {
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.fechacontra = Calendar.getInstance();
+        this.salariobase = salariobase;
+        this.horastrabajadas = 0;
+    }
+
+    public double Horastrabajadas(double horas) {
+        if (horas > 0) {
+            return horastrabajadas += horas;
+        } else {
+            return horastrabajadas = horastrabajadas;
+        }
+
+    }
+
+    public double CalculoPago(double horastrabajadas) {
+        double salarioproporcional = horastrabajadas / 160;
+        double descuento = salarioproporcional * 0.035;
+        return salarioproporcional - descuento;
+    }
+
+    public void MostrarInfo() {
+        System.out.println("Codigo del empleado: " + codigo + " Nombre del Empleado:" + nombre + "Fecha de contratacion: " + fechacontra.getTime());
+
+    }
 }
